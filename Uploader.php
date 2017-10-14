3fd018dd5b21d1ac7691d7f27e967a57ef0baa36
dankunis <dankunis@github.com>
2017-10-14 14:42:06 +0300

    Все исправил
    Все исправил, следуя вашим комментам, а также добавил обработку ситуации, когда человек потверждает форму, ничего не написав или не загрузив файл.
    
    PS: Так и не нашел решения про то, как получить разммер удаленного файла.

--- Uploader.php
+++ Uploader.php
@@ -1,44 +1,55 @@
 <?php
-const MaxFileSize = 5000000000000;
-#Сейчас поставил макcимальный размер файла,который можно загрузить на Amazon - 5TB. Жду от вас нормального размера.
-const EXPANSIONS = array('image/jpg', 'image/png', 'image/jpeg', 'image/gif', 'jpg', 'png', 'jpeg', 'gif');
+const MAXFILESIZE = 125829120; #15 МБ
+const EXTENSIONS = array('image/jpg', 'image/png', 'image/jpeg', 'image/gif', 'jpg', 'png', 'jpeg', 'gif');
 ?>
-<form method="post" enctype="multipart/form-data" action="">
-    <input type="file" name="image"/>
-    <input type="submit" name="FileSubm" value="Accept"/>
-</form>
+    <form method="post" enctype="multipart/form-data" action="">
+        <input type="file" name="ImageFile"/>
+        <input type="submit" name="FileSubm" value="Accept"/>
+    </form>
 <?php
-if(isset($_POST['FileSubm'])) {
-    $FileName = $_FILES['image']['name'];
-    $FileExp = $_FILES['image']['type'];
-    $FileSize = $_FILES['image']['size'];
-    if (!in_array($FileExp, EXPANSIONS)) {
-        echo "Wrong file type.";
+if(isset($_FILES['ImageFile'])) {
+    if(empty($_FILES['ImageFile']['name'])) {
+        echo "Please, select a file.";
     }
-    elseif ($FileSize > MaxFileSize) {
-        echo "The file is too big.";
+    else {
+        if (isset($_FILES['ImageFile']['name'])) {
+            $FileName = $_FILES['ImageFile']['name'];
+        }
+        if (isset($_FILES['ImageFile']['type'])) {
+            $FileExp = $_FILES['ImageFile']['type'];
+        }
+        if (isset($_FILES['ImageFile']['size'])) {
+            $FileSize = $_FILES['ImageFile']['size'];
+        }
+        if (isset($FileExp) and !in_array($FileExp, EXTENSIONS)) {
+            echo "Wrong file type.";
+        } elseif (isset($FileSize) and $FileSize > MAXFILESIZE) {
+            echo "The file is too big.";
+        } else {
+            #Вызов функции putObject из storage.php
+            echo "Success";
+        }
+    }
+}
+?>
+    <form method="get" action="">
+        <input type="text" name="ImageLink"/>
+        <input type="submit" name="LinkSubm" value="Accept"/>
+    </form>
+<?php
+if(!empty($_GET['ImageLink'])) {
+    $FileName = basename($_GET['ImageLink']);
+    $FileExp = explode('.', $_GET['ImageLink']);
+    $FileExp = $FileExp[count($FileExp)-1];
+    if (!in_array($FileExp, EXTENSIONS)) {
+        echo "Wrong file type.";
     }
     else {
         #Вызов функции putObject из storage.php
         echo "Success";
     }
 }
-?>
-<form method="get" action="">
-    <input type="text" name="FileLink" value="Put your link here"/>
-    <input type="submit" value="Accept"/>
-</form>
-<?php
-if($_GET['FileLink']) {
-    $FileName = basename($_GET['FileLink']);
-    $FileExp = explode('.', $_GET['FileLink']);
-    $FileExp = $FileExp[count($FileExp)-1];
-    if (!in_array($FileExp, EXPANSIONS)) {
-        echo "Wrong file type.";
-    }
-    else {
-        #Вызов функции putObject из storage.php
-        echo "Success";
-    }
+else {
+    echo "Please, enter a link.";
 }
 ?>
