8a24bba430afd94d729736fd3057f54c023a814c
Gollut <gollut9@gmail.com>
2017-10-16 23:39:29 +0300

    Empty strings

--- lib/router/router.php
+++ lib/router/router.php
@@ -1,5 +1,7 @@
 <?php
+
 require 'Dispatcher.php';
+
 // Dictionary of supported filters and their patterns
 const FILTERS = array(
     'c' => array ('title' => 'crop', 'pattern' => '{w|int}x{h|int}[&{x|int},{y|int}]'),
