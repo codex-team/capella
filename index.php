d4a584012693dff5e6154d2b7edef8d56888560c
Taly <vitalik7tv@yandex.ru>
2017-10-17 20:13:20 +0300

    First version

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 
 require_once "vendor/autoload.php";
 
-/* Sample AWS Storage usage
-require_once 'lib/AWS/Storage.php';
-$storage = new AWS_Storage();
-$imgID = $storage->uploadImage('nice-pic.png');
-$imgURI = $storage->getImage($imgID);
-echo $imgURI;
-*/
+require_once "lib/autoload.php";
+
+/**
+ * Main page
+ */
+require_once "Uploader.php";
 
 ?>
