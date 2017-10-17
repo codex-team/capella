d62696f0fedd1d77a782928054c823f0b420d743
Gollut <gollut9@gmail.com>
2017-10-17 19:43:33 +0300

    Connected with Delivery

--- lib/router/router.php
+++ lib/router/router.php
@@ -1,14 +1,16 @@
 <?php
 
-require 'Dispatcher.php';
+require_once 'Dispatcher.php';
+require_once '../lib/Delivery.php';
 
 // Dictionary of supported filters and their patterns
 const FILTERS = array(
-    'c' => array ('title' => 'crop', 'pattern' => '{w|int}x{h|int}[&{x|int},{y|int}]'),
-    'r' => array ('title' => 'resize', 'pattern' => '{w}x{h}'),
+    'c' => array('title' => 'crop', 'pattern' => '{w|int}x{h|int}[&{x|int},{y|int}]'),
+    'r' => array('title' => 'resize', 'pattern' => '{w}x{h}'),
 );
 $uri = $_SERVER['REQUEST_URI'];
 
-$dispatcher  = new UriDispatcher($uri, FILTERS);
-$imageData   = array('id' => $dispatcher->id, 'filters' => $dispatcher->parsedFilters);
-// Call image -> show($data);
+$dispatcher = new UriDispatcher($uri, FILTERS);
+$imageData  = array('id' => $dispatcher->id, 'filters' => $dispatcher->parsedFilters);
+$image      = new Delivery($imageData['id'], $imageData['filters']);
+$image->returnImage();
