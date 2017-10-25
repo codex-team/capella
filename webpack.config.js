dd53f7d9390cac5308c769dd634ae26550cd893e
root <mister.0025@yandex.ru>
2017-10-25 20:05:30 +0000

    Css loaders added

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,37 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
-    entry: './public/app/js/main.js',
+    entry: './public/app/main.js',
     module: {
         rules: [
             {
                 test: /.js$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use: ['babel-loader', 'eslint-loader']
+            },
+            {
+                test: /.css$/,
+                exclude: /node_modules/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
             }
         ]
     },
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'public', 'build'),
         filename: 'bundle.js'
-    }
+    },
+    watchOptions: {
+        aggregateTimeOut: 100
+    },
+    devtool: NODE_ENV === 'development' ? 'source-map' : false,
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
 };
