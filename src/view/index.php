7a95b7de96841a2ee59410c622b9b2e67f4dfd9d
Polina Shneider <beatrixlinkoln@gmail.com>
2017-10-31 00:31:18 +0300

    Pixel-perfect
    At least, close to :)

--- src/view/index.php
+++ src/view/index.php
@@ -1,28 +1,30 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Main</title>
-    <link rel="stylesheet" type="text/css" href="../public/build/bundle.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-</head>
-<body>
-    <div class="capella-background">
-        <div class="capella">
-            <div class="capella__contents">
-                <img src="../public/app/img/Logo.png">
-                <div class="capella__title">capella</div>
-                <p>Upload file directly using drag & drop or clipboard.<br>You will instantly get link to your file.</p>
-                <form method="post" action="/upload" enctype="multipart/form-data">
-                    <label for="ImageFile" class="capella__button">Select picture</label>
-                    <input id="ImageFile" type="file" name="file" hidden="true"/>
-                    <input type="submit" value="Upload" hidden="true"/>
-                </form>
-                <form method="post" action="/upload" autocomplete="off">
-                    <input class="capella__input" type="text" name="link" placeholder="Paste the URL"/>
-                    <input type="submit" value="Upload" hidden="true"/>
-                </form>
+    <head>
+        <title>Main</title>
+        <link rel="stylesheet" type="text/css" href="../public/build/bundle.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    </head>
+    <body>
+        <div class="capella-background">
+            <div class="capella">
+                <div class="capella__contents">
+                    <img src="../public/app/img/Logo.svg">
+                    <div class="capella__title">
+                        <img src="../public/app/img/Capella-branding.svg">
+                    </div>
+                    <p>Upload file directly using drag-n-drop or clipboard. You will instantly get link to your file.</p>
+                    <form method="post" action="/upload" enctype="multipart/form-data">
+                        <label for="ImageFile" class="capella__button">Select picture</label>
+                        <input id="ImageFile" type="file" name="file" hidden="true"/>
+                        <input type="submit" value="Upload" hidden="true"/>
+                    </form>
+                    <form method="post" action="/upload" autocomplete="off">
+                        <input class="capella__input" type="text" name="link" placeholder="Paste the URL"/>
+                        <input type="submit" value="Upload" hidden="true"/>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-</body>
+    </body>
 </html>
