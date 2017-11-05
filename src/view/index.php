4f3100e28fe68b0676a622444f1eb3c3d79eab5c
Polina Shneider <beatrixlinkoln@gmail.com>
2017-11-05 19:48:53 +0300

    Optimized svg
    Created folder for svg
    Deleted unused svg
    CSS properties order refactoring

--- src/view/index.php
+++ src/view/index.php
@@ -2,6 +2,9 @@
 <html>
     <head>
         <title>Main</title>
+        <link rel="icon" href="./favicon.ico" type="image/x-icon">
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+        <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="../public/build/bundle.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </head>
@@ -11,7 +14,7 @@
                 <div class="capella__contents">
                     <div class="capella__logo"></div>
                     <div class="capella__title">
-                        <img src="../public/app/img/Capella-branding.svg">
+                        <img src="../public/app/svg/Capella-branding.svg">
                     </div>
                     <p>Upload file directly using drag-n-drop or clipboard. You will instantly get link to your file.</p>
                     <button id="uploadFileButton" class="capella__button">Select picture</button><br>
