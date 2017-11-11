b18a9a16d89f7ec2c0b8e9257001dbfadc4a2249
Polina Shneider <beatrixlinkoln@gmail.com>
2017-11-11 20:09:09 +0300

    Background, finally

--- src/view/index.php
+++ src/view/index.php
@@ -17,7 +17,7 @@
                     </div>
                     <p>Upload file directly using drag-n-drop or clipboard. You will instantly get link to your file.</p>
                     <button id="uploadFileButton" class="capella__button">Select picture</button><br>
-                    <input id="uploadLinkField" type="text" class="capella__input" placeholder="Paste the URL"/>
+                    <input id="uploadLinkField" type="text" class="capella__input" placeholder="Paste URL"/>
                 </div>
             </div>
         </div>
