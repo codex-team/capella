f1181509cc6e16d720f91ff0bc0b029698a0a40a
Polina Shneider <beatrixlinkoln@gmail.com>
2017-11-16 20:44:43 +0300

    favicon

--- src/view/index.php
+++ src/view/index.php
@@ -4,6 +4,7 @@
         <title>Main</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="../public/build/bundle.css">
+        <link rel="icon" type="image/png" href="/favicon.png">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </head>
     <body>
