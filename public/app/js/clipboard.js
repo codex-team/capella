5311f0460ff3dda9dfa295a3120bb91693c9a961
Nikita <nikitos@MacBook-Pro.local>
2017-11-25 20:36:02 +0300

    Clipboard global changes

--- public/app/js/clipboard.js
+++ public/app/js/clipboard.js
@@ -16,22 +16,32 @@ export default class Clipboard {
     /**
      * items - data from clipboard
      */
-    let items = (event.clipboardData  || event.originalEvent.clipboardData).items;
+    let items = (event.clipboardData  || event.originalEvent.clipboardData || window.clipboardData).items;
+    let data = (event.clipboardData  || event.originalEvent.clipboardData || window.clipboardData).getData('Text');
     let blob = null;
 
-    /**
-     * Checking all clipboard's files and choosing last image file
-     */
-    for (let i = items.length - 1; i >= 0; --i) {
-      if (items[i].type.indexOf('image') === 0) {
-        blob = items[i].getAsFile();
-        break;
-      }
-      if (items[i].type.indexOf('text') === 0) {
-        let input = event.clipboardData.getData('Text');
+    // Stop data actually being pasted into document {default settings}
+    event.stopPropagation();
+    event.preventDefault();
 
-        if (input.match(/^((http[s]?):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/)) {
-          capella.uploader.upload({'link': input});
+    /*
+     * Checking if clipboard has a link
+     */
+    if (data) {
+      /**
+       * Parsing on valid URL
+       */
+      if (data.match(/^((http[s]?):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/)) {
+        capella.uploader.upload({'link': data});
+      }
+    } else if (items) {
+      /**
+       * Checking all clipboard's files and choosing last image file
+       */
+      for (let i = items.length - 1; i >= 0; --i) {
+        if (items[i].type.indexOf('image') === 0) {
+          blob = items[i].getAsFile();
+          break;
         }
       }
     }
