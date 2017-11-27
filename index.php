24864abb2706605c3da8720e9ba5b608202da710
Taly <vitalik7tv@yandex.ru>
2017-11-27 14:53:56 +0300

    Add check environment file

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
 define('DOCROOT', realpath(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 
 /**
+ * Load the installation check
+ */
+if (file_exists('install.php'))
+{
+    return include 'install.php';
+}
+
+/**
  * Bootstrap the application
  */
 require DOCROOT.'src/bootstrap.php';
