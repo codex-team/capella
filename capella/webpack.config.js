1e486e7266bee4025afed0909d047f9ab9f1f13b
Taly <vitalik7tv@yandex.ru>
2020-02-07 17:09:22 +0300

    Upgrade styles config, remove unnesessary packages (#137)

--- capella/webpack.config.js
+++ capella/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   /**
